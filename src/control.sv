36bc531487ca41720be0bbdbb900aeb60488ebc2
Samuel Devotta <samuel.devotta@gmail.com>
2026-07-08 16:04:03 +0100

    Modified control to be able to handle R-type instructions, added alu_src and result_src to act as multiplexers

--- src/control.sv
+++ src/control.sv
@@ -14,7 +14,9 @@ module control(
     output logic [2:0] alu_control,
     output logic [1:0] imm_source,
     output logic mem_write,
-    output logic reg_write
+    output logic reg_write,
+    output logic alu_src,
+    output logic result_src
 );
 
 // MAIN DECODER
@@ -28,6 +30,8 @@ always_comb begin
             imm_source = 2'b00;
             mem_write = 1'b0;
             alu_op = 2'b00;
+            alu_src = 1'b1; // imm read
+            result_src = 1'b1; // memory write
         end
 
         //SW
@@ -36,7 +40,19 @@ always_comb begin
             imm_source = 2'b01;
             mem_write = 1'b1;
             alu_op = 2'b00;
+            alu_src = 1'b1; //imm read
         end
+
+        // ADD
+        7'b0110011 : begin
+            reg_write = 1'b1;
+            imm_source = 2'bxx;
+            mem_write = 1'b0;
+            alu_op = 2'b10;
+            alu_src = 1'b0; // register read
+            result_src = 1'b0; // alu read
+        end
+
         // Everything else
         default : begin
             reg_write = 1'b0;
@@ -53,6 +69,18 @@ always_comb begin
     case(alu_op)
         // LW, SW
         2'b00 : alu_control = 3'b000;
+
+        // R-type instructions
+        2'b10 : begin 
+            case(func3)
+                3'b000 : if(func7[5] == 1'b0) 
+                            alu_control = 3'b000;
+                         else 
+                            alu_control = 3'b001;
+                default : alu_control = 3'b111;
+            endcase
+        end
+
         //Everything else
         default : alu_control = 3'b111;
     endcase
