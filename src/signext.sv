0997720013d02959b637f6c96b68a4ec16cd428a
Samuel Devotta <samuel.devotta@gmail.com>
2026-07-15 11:59:35 +0100

    Added compatability with imm_source = 10

--- src/signext.sv
+++ src/signext.sv
@@ -19,10 +19,12 @@ always_comb begin
         2'b00 : gathered_imm = raw_src[24:13];
         // S-types
         2'b01 : gathered_imm = {raw_src[24:18],raw_src[4:0]};
+        // B-tpyes
+        2'b10 : gathered_imm = {raw_src[0], raw_src[23:18], raw_src[4:1], 1'b0};
         default : gathered_imm = 12'b0;
     endcase
 end
 
-assign immediate = {{20{gathered_imm[11]}}, gathered_imm};
+assign immediate = (imm_source == 2'b10) ? {{20{raw_src[24]}}, gathered_imm} : {{20{gathered_imm[11]}}, gathered_imm};
 
 endmodule
